79a6f139f696b264f9ccf065eb80da4be1a8e0ff
Juan Linares <130109926+SrJuanPi@users.noreply.github.com>
2024-06-26 14:40:00 -0400

    Python projects v1 June 26, 2024

---
+++ Proyectos.py
@@ -0,0 +1,34 @@
+# Resumen de mis 5 proyectos hasta ahora
+
+from Python import Calculadora as cl,IMC as imc,Rentabilidad as rent,Temperaturas as temp,Velocidad as v
+
+print()
+print("Bienvenido, mi nombre es Juan y eh estado aprendiendo a programar en Python últimamente")
+print("Estos son 5 de mis proyectos más recientes hasta ahora, son programas sencillos pero funcionales")
+print("Cabe aclarar que no son programas perfectos, pueden haber pequeños errores.")
+print()
+
+print("Para mi primer proyecto he creado una Calculadora bastante sencilla, veamos como funciona.")
+print()
+cl.calculadora()
+print()
+
+print("Para mi segundo proyecto he creado un Conversor de Temperatura, veamos como funciona.")
+print()
+temp.temperatura()
+print()
+
+print("Mi tercer proyecto lo hice con la intención de moderar mis compras, se trata de un Calculador de Rentabilidad.")
+print()
+rent.rentabilidad()
+print()
+
+print("En esta ocasión quería saber como estaba mi estado físico actual, y para ello creé una Calculadora IMC.")
+print()
+imc.imc()
+print()
+
+print("Y por último tenemos un Calculador de Velocidad.")
+print()
+v.velocidad()
+print()
