a16459cb5487fa6378d389150d22e4c726930fe7
SrJuanPi <juanpablolinareseijas@gmail.com>
2024-12-01 16:00:58 -0300

    New program: To do list with json files

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-import json
+import json, re
 
 def to_dict(jsonfile):
     with open(jsonfile, 'r') as file:
@@ -14,33 +14,92 @@ def add(dic, key):
     return dic
 
 def rename(dic, old, new):
-    dic[f"{new}"] = dic.pop(f"{old}")
+    dic[new] = dic.pop(old)
     return dic
 
 def completed(dic, key):
-    dic[f"{key}"] = "completed"
+    dic[key] = "completed"
     return dic
     
 def view_list(dic):
     to_str = str(dic)
-    formated = to_str.replace("{", "\n ").replace("}", "").replace("'", "").replace(",", "\n")
-    return formated    
+    formatted = to_str.replace("{", "\n ").replace("}", "").replace("'", "").replace(",", "\n")
+    return formatted    
+
+def save_list(dic, jsonfile):
+    with open(jsonfile, 'w') as file:
+        json.dump(dic, file, indent=4)
 
 
 def main(): 
     print("To Do List Today")
     print("""
+    0) Exit
     1) View To do list
     2) Mark Element as completed
     3) Remove an Element list
     4) Add a new Element to the list
     5) Rename Element
-    6) Save list (coming soon)
-    7) Exit
-        """)
-    jsonfile = input("Enter the list with .json: ")
-    jsonfile = to_dict(jsonfile)
-    print(view_list(jsonfile))
+    6) Save list
+    """)
+    
+    pattern = r'^[\w,\s-]+\.[Jj][Ss][Oo][Nn]$'
+    while True:
+        jsonfile = input("Enter the list with .json: ")
+        if re.match(pattern, jsonfile):
+            try: tasks = to_dict(jsonfile)
+            except FileNotFoundError:
+                print("Enter an existing .json file.")
+                continue
+            else: break
+        else: print("Invalid file format. Please enter a valid .json file.")
+    print(view_list(tasks))
+    
+    while True:
+        option = input("\nSelect an option: ")
+        if option == "0":
+            break
+        elif option == "options":
+            print("""
+    0) Exit
+    1) View To do list
+    2) Mark Element as completed
+    3) Remove an Element list
+    4) Add a new Element to the list
+    5) Rename Element
+    6) Save list
+    """)
+        
+        elif option == "1":
+            print(view_list(tasks))
+            
+        elif option == "2":
+            mark_completed = input("enter the name task to mark completed: ")
+            tasks = completed(tasks, mark_completed)
+            print(view_list(tasks))
+            
+        elif option == "3":
+            element = input("element to remove: ")
+            tasks = remove(tasks, element)
+            print(view_list(tasks))
+            
+        elif option == "4":
+            element = input("enter the name of element to add: ")
+            tasks = add(tasks, element)
+            print(view_list(tasks))
+            
+        elif option == "5":
+            old = input("enter the element to rename: ")
+            new = input("entre the new name: ")
+            tasks = rename(tasks, old, new)
+            print(view_list(tasks))
+        
+        elif option == "6":
+            save_list(tasks, jsonfile)
+            print("\nList saved!")
+        
+        else:
+            print('Options available are numbers from 1 to 6 (0 for exit and "options" to view options again).')
     
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,20 +1,49 @@
 from list import *
+
+# jsonfile = input("insert json file: ")
+# tasks = to_dict(jsonfile)
+
+# while True:
+#     option = input("\nEnter a number (1-6 (7 to exit)) : ")
     
-jsonfile = input("insert json file: ")
-tasks = to_dict(jsonfile)
+#     if option == "7":
+#         break
+    
+#     elif option == "1":
+#         print(view_list(tasks))
+        
+#     elif option == "2":
+#         mark_completed = input("enter the name task to mark completed: ")
+#         new_list = completed(tasks, mark_completed)
+#         print(view_list(new_list))
+        
+#     elif option == "3":
+#         element = input("element to remove: ")
+#         new_list = remove(tasks, element)
+#         print(view_list(new_list))
+        
+#     elif option == "4":
+#         element = input("enter the name of element to add: ")
+#         new_list = add(tasks, element)
+#         print(view_list(new_list))
+        
+#     elif option == "5":
+#         old = input("enter the element to rename: ")
+#         new = input("entre the new name: ")
+#         new_list = rename(tasks, old, new)
+#         print(view_list(new_list))
+        
+#     elif option == "6":
+#         print("Coming Soon...")
+    
+#     else: continue
+
 
 while True:
-    option = input("\nEnter a number (1-7): ")
-    
-    if option == "exit":
-        break
-    
-    elif option == "1":
-        print(view_list(tasks))
-        
-    elif option == "2":
-        value_to_remove = input("enter the name taks to remove: ")
-        dic = completed(tasks, value_to_remove)
-        print(view_list(dic))
-        
-    else: continue
+    jsonfile = input("Enter the list with .json: ")
+    try: tasks = to_dict(jsonfile)
+    except:
+        print("Try again.")
+        continue
+    else: break
+print(view_list(tasks))
