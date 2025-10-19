b5d323f14197feab8d7813956ef40489da53720a
SrJuanPi <juanpablolinareseijas@gmail.com>
2025-10-18 22:03:08 -0300

    Reviewing Python 3.14 update

---
+++ test-3_13.py
@@ -0,0 +1,9 @@
+
+name: str = "hola" # No sabía que esto se podía hacer xd
+a = t"Esto es una plantilla, me llamo {name}" # Error en 3.13
+
+for i in range(1,10001):
+    print(i)
+    i += 1
+
+# test 3.13 aprox 7-10s
